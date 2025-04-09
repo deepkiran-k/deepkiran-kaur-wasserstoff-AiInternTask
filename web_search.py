fa166d0b4e5f54bf1653fa4774e837896e81ae56
Deepkiran Kaur <140378743+deepkiran-k@users.noreply.github.com>
2025-04-09 17:03:28 +0530

    Add files via upload

---
+++ web_search.py
@@ -0,0 +1,12 @@
+import requests
+
+def web_search(google_api_key, cse_id, query, num_results=3):
+    url = "https://www.googleapis.com/customsearch/v1"
+    params = {
+        'key': google_api_key,
+        'cx': cse_id,
+        'q': query,
+        'num': num_results
+    }
+    response = requests.get(url, params=params).json()
+    return [item['snippet'] for item in response.get('items', [])]
